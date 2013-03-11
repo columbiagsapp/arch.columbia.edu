4cf09a232449eb348c132ff9cf4389e7dc512f80
Troy Conrad Therrien <troyth@gmail.com>
2013-03-11 12:28:24 -0400

    sync with server

--- sites/all/themes/tmpltzr/node-widgetupcomingevents.tpl.php
+++ sites/all/themes/tmpltzr/node-widgetupcomingevents.tpl.php
@@ -12,7 +12,7 @@
 
 
 
-<div id="node-<?php print $node->nid; ?>" class="tmpltzr-module tmpltzr-module-240<?php if($color){ print ' '.$color; } ?> tmpltzr-widget tmpltzr-upcomingeventswidget tmpltzr-fetched node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?><?php if ($color) { print ' '.$color; } ?> clearfix">
+<div id="node-<?php print $node->nid; ?>" class="tmpltzr-module tmpltzr-secondary-float tmpltzr-module-240<?php if($color){ print ' '.$color; } ?> tmpltzr-widget tmpltzr-upcomingeventswidget tmpltzr-fetched node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?><?php if ($color) { print ' '.$color; } ?> clearfix">
 	<a id="<?php print $node->title; ?>" name="<?php print $node->title; ?>" class="anchorhash"></a>
 	<h4 style="text-align:center;margin:0 auto 25px;">Upcoming Events</h4>
   	<?php
@@ -22,7 +22,14 @@
 			'gsappFetcher.getUpcomingEventsWidget("'. $url . '?callback=?","#upcomingeventswidget-output");' .
 			'</script>';
   	?>
-  	<h4 style="text-align:center;margin:0 auto;text-transform:none;"><a href="http://events.gsapp.org/" target="_blank">View More</a></h4>
+  	<h4 style="text-align:center;margin:0 auto;text-transform:none;">
+  		<?php
+  			if(!empty($node->field_more_link)){
+  				print $node->field_more_link[0]['view'];
+  			}
+  		?>
+
+  	</h4>
 
 <?php printEditSectionFooter($user->uid, $node->uid, $node->nid, node_url); ?>
 </div> <!-- /.node -->
